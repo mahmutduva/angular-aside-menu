5da3e9e55ed9e89be80e8e79d5a55ccae3cd352c
Mahmut Duva <mahmut.duva@freebirdairlines.com>
2015-12-13 17:41:36 +0200

    remove some functions

--- dist/aside-menu.js
+++ dist/aside-menu.js
@@ -9,165 +9,247 @@
  * Licensed under the MIT license
  */
 
-/*jslint unparam: true */
-/*global angular: false, console: false, define, module */
-(function (root, factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['angular'], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        // to support bundler like browserify
-        module.exports = factory(require('angular'));
-    } else {
-        // Browser globals (root is window)
-        factory(root.angular);
-    }
 
-}(this, function (angular) {
-    'use strict';
-    var module = angular.module('asideModule', [])
-        .directive('asideMenuToggle', ['$compile', function ($compile) {
-            'use strict';
-
-            return {
-                restrict: 'EA',
-                link: function (scope, elem, attrs) {
-                    scope.isOpen = false;
-                    scope.menus = document.getElementsByTagName("aside-menu");
-                    scope.menuContent = document.getElementsByClassName("aside-menu-content");
-
-                    scope.targetMenu = {
-                        "item":null,
-                        "width": 275,
-                        "side" : "left",
-                        "pushContentContent": false,
-                        "isBackdrop": false
-                    };
-
-                    elem.bind('click', function () {
-
-                        // reset menu content transform
-                        angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
-
-                        angular.forEach(scope.menus, function (item) {
-
-                            // Close all menus
-                            angular.element(item).css("transform", "translate3d(0px, 0px, 0px)");
-
-                            // get target menu options from attrs
-                            if (angular.element(item).attr("id") == attrs.asideMenuToggle) {
-                                scope.targetMenu.item = angular.element(item);
-                                scope.targetMenu.width = angular.element(item).attr("width");
-                                scope.targetMenu.side = angular.element(item).attr("side");
-                                scope.targetMenu.pushContent = angular.element(item).attr("push-content");
-                                scope.targetMenu.isBackdrop = angular.element(item).attr("is-backdrop");
-                            }
-                        });
+'use strict';
+var module = angular.module('asideModule', [])
 
 
-                        if (!scope.isOpen) {
-                            if (scope.targetMenu.side == "left") {
-                                if (scope.targetMenu.pushContent == "true") {
-                                    angular.element(scope.menuContent).css("transform", "translate3d(" + scope.targetMenu.width + "px, 0px, 0px)");
-                                }
-                                else {
-                                    scope.targetMenu.item.css("transform", "translate3d(" + scope.targetMenu.width + "px, 0px, 0px)");
-                                }
-                            }
-                            else if (scope.targetMenu.side == "right") {
-                                if (scope.targetMenu.pushContent == "true") {
-                                    angular.element(scope.menuContent).css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
+    .directive('asideMenuToggle', ['$compile', function ($compile) {
+        'use strict';
 
-                                }
-                                else {
-                                    scope.targetMenu.item.css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
-                                }
+        return {
+            restrict: 'EA',
+            link: function (scope, elem, attrs) {
 
-                            }
-                            if(scope.targetMenu.isBackdrop == "true"){
-                                var el = $compile('<div close-aside-menu class="aside-back-drop fade"></div>')( scope );
-                                angular.element(scope.menuContent).append(el);
 
-                            }
+                // is open menu
+                scope.isOpen = false;
 
-                            scope.isOpen  = true;
 
+                // All Menus
+                scope.menus = document.getElementsByTagName("aside-menu");
+
+
+                // Menu Content
+                scope.menuContent = document.getElementsByClassName("aside-menu-content");
+
+
+                // Default menu Options
+                scope.targetMenu = {
+                    "item": null,
+                    "width": 275,
+                    "side": "left",
+                    "pushContentContent": false,
+                    "isBackdrop": false
+                };
+
+
+                // Click Event
+                elem.bind('click', function () {
+
+                    // reset menu content transform
+                    angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
+
+
+                    // List all menus
+                    angular.forEach(scope.menus, function (item) {
+
+
+                        // Close menu
+                        angular.element(item).css("transform", "translate3d(0px, 0px, 0px)");
+
+
+                        // Get target menu options from attrs
+                        if (angular.element(item).attr("id") == attrs.asideMenuToggle) {
+                            scope.targetMenu.item = angular.element(item);
+                            scope.targetMenu.width = angular.element(item).attr("width");
+                            scope.targetMenu.side = angular.element(item).attr("side");
+                            scope.targetMenu.pushContent = angular.element(item).attr("push-content");
+                            scope.targetMenu.isBackdrop = angular.element(item).attr("is-backdrop");
                         }
-                        else {
+
+
+                    });
+
+
+                    if (!scope.isOpen) {
+
+
+                        if (scope.targetMenu.side == "left") {
+
+
                             if (scope.targetMenu.pushContent == "true") {
-                                angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
+
+
+                                angular.element(scope.menuContent).css("transform", "translate3d(" + scope.targetMenu.width + "px, 0px, 0px)");
+
 
                             }
                             else {
-                                scope.targetMenu.item.css("transform", "translate3d(0px, 0px, 0px)");
+
+
+                                scope.targetMenu.item.css("transform", "translate3d(" + scope.targetMenu.width + "px, 0px, 0px)");
+
+
                             }
-                            scope.isOpen  = false;
+                        }
+                        else if (scope.targetMenu.side == "right") {
+
+
+                            if (scope.targetMenu.pushContent == "true") {
+
+
+                                angular.element(scope.menuContent).css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
+
+
+                            }
+                            else {
+
+
+                                scope.targetMenu.item.css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
+
+
+                            }
+
+                        }
+                        if (scope.targetMenu.isBackdrop == "true") {
+
+
+                            var el = $compile('<div close-aside-menu class="aside-back-drop fade"></div>')(scope);
+
+                            angular.element(scope.menuContent).append(el);
+
+
                         }
 
-                    });
-                }
-            };
-        }])
-        .directive('asideMenu', function () {
-            'use strict';
+                        scope.isOpen = true;
 
-            return {
-                restrict: 'EA',
-                link: function (scope, elem, attrs) {
-                    angular.element(elem).addClass("aside-menu aside-menu-animate");
-                    angular.element(elem).css({
-                        "width": attrs.width + "px"
-                    });
-                    if (attrs.side == "left") {
-                        angular.element(elem).css("left", 0);
-                        if (attrs.pushContent == "false") {
-                            angular.element(elem).css("z-index", "99");
-                            angular.element(elem).css("left", "-" + attrs.width + "px");
-                        }
                     }
-                    else if (attrs.side == "right") {
-                        angular.element(elem).css("right", 0);
-                        if (attrs.pushContent == "false") {
-                            angular.element(elem).css("z-index", "99");
-                            angular.element(elem).css("right", "-" + attrs.width + "px");
+                    else {
+
+
+                        if (scope.targetMenu.pushContent == "true") {
+
+
+                            angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
+
+
                         }
-                    }
+                        else {
 
-                }
-            };
-        })
-        .directive('asideMenuContent', function () {
-            'use strict';
 
-            return {
-                restrict: 'AE',
-                scope: true,
-                link: function (scope, elem, attrs) {
-                    angular.element(elem).addClass('aside-menu-content aside-menu-animate close-aside-menu');
-                }
-            };
-        })
-        .directive('closeAsideMenu', function () {
-            'use strict';
+                            scope.targetMenu.item.css("transform", "translate3d(0px, 0px, 0px)");
 
-            return {
-                restrict: 'AC',
-                link: function (scope, elem, attrs) {
-                    elem.bind('click',function(){
-                        angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
-                        scope.targetMenu.item.css("transform", "translate3d( 0px, 0px, 0px)");
+
+                        }
+
                         scope.isOpen = false;
-                        elem.remove();
-                    })
+                    }
+
+                });
+            }
+        };
+    }])
+
+
+    .directive('asideMenu', function () {
+        'use strict';
+
+        return {
+            restrict: 'EA',
+            link: function (scope, elem, attrs) {
+
+                angular.element(elem).addClass("aside-menu aside-menu-animate");
+
+
+                angular.element(elem).css({"width": attrs.width + "px"});
+
+
+                if (attrs.side == "left") {
+
+
+                    angular.element(elem).css("left", 0);
+
+
+                    if (attrs.pushContent == "false") {
+
+
+                        angular.element(elem).css("z-index", "99");
+
+                        angular.element(elem).css("left", "-" + attrs.width + "px");
+
+
+                    }
+
+
                 }
-            };
-        })
+                else if (attrs.side == "right") {
 
 
+                    angular.element(elem).css("right", 0);
 
-    return module
-}));
+
+                    if (attrs.pushContent == "false") {
+
+
+                        angular.element(elem).css("z-index", "99");
+
+                        angular.element(elem).css("right", "-" + attrs.width + "px");
+
+
+                    }
+
+
+                }
+
+
+            }
+        };
+    })
+
+
+    .directive('asideMenuContent', function () {
+        'use strict';
+
+        return {
+            restrict: 'AE',
+            scope: true,
+            link: function (scope, elem, attrs) {
+
+
+                angular.element(elem).addClass('aside-menu-content aside-menu-animate close-aside-menu');
+
+
+            }
+        };
+    })
+
+
+    .directive('closeAsideMenu', function () {
+        'use strict';
+
+        return {
+            restrict: 'AC',
+            link: function (scope, elem, attrs) {
+
+
+                elem.bind('click', function () {
+
+
+                    angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
+
+                    scope.targetMenu.item.css("transform", "translate3d( 0px, 0px, 0px)");
+
+                    scope.isOpen = false;
+
+                    elem.remove();
+
+
+                })
+
+
+            }
+        };
+    });
+
+
--- src/aside-menu.js
+++ src/aside-menu.js
@@ -9,233 +9,247 @@
  * Licensed under the MIT license
  */
 
-/*jslint unparam: true */
-/*global angular: false, console: false, define, module */
-(function (root, factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['angular'], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        // to support bundler like browserify
-        module.exports = factory(require('angular'));
-    } else {
-        // Browser globals (root is window)
-        factory(root.angular);
-    }
 
-}(this, function (angular) {
-    'use strict';
-    var module = angular.module('asideModule', [])
+'use strict';
+var module = angular.module('asideModule', [])
 
 
-        .directive('asideMenuToggle', function ($compile) {
-            'use strict';
+    .directive('asideMenuToggle', function ($compile) {
+        'use strict';
 
-            return {
-                restrict: 'EA',
-                link: function (scope, elem, attrs) {
-
-
-                    // is open menu
-                    scope.isOpen = false;
+        return {
+            restrict: 'EA',
+            link: function (scope, elem, attrs) {
 
 
-                    // All Menus
-                    scope.menus = document.getElementsByTagName("aside-menu");
+                // is open menu
+                scope.isOpen = false;
 
 
-                    // Menu Content
-                    scope.menuContent = document.getElementsByClassName("aside-menu-content");
+                // All Menus
+                scope.menus = document.getElementsByTagName("aside-menu");
 
 
-                    // Default menu Options
-                    scope.targetMenu = {
-                        "item": null,
-                        "width": 275,
-                        "side": "left",
-                        "pushContentContent": false,
-                        "isBackdrop": false
-                    };
+                // Menu Content
+                scope.menuContent = document.getElementsByClassName("aside-menu-content");
 
 
-                    // Click Event
-                    elem.bind('click', function () {
-
-                        // reset menu content transform
-                        angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
-
-
-                        // List all menus
-                        angular.forEach(scope.menus, function (item) {
-
-
-                            // Close menu
-                            angular.element(item).css("transform", "translate3d(0px, 0px, 0px)");
-
-                            // Get target menu options from attrs
-                            if (angular.element(item).attr("id") == attrs.asideMenuToggle) {
-                                scope.targetMenu.item = angular.element(item);
-                                scope.targetMenu.width = angular.element(item).attr("width");
-                                scope.targetMenu.side = angular.element(item).attr("side");
-                                scope.targetMenu.pushContent = angular.element(item).attr("push-content");
-                                scope.targetMenu.isBackdrop = angular.element(item).attr("is-backdrop");
-                            }
+                // Default menu Options
+                scope.targetMenu = {
+                    "item": null,
+                    "width": 275,
+                    "side": "left",
+                    "pushContentContent": false,
+                    "isBackdrop": false
+                };
 
 
-                        });
+                // Click Event
+                elem.bind('click', function () {
+
+                    // reset menu content transform
+                    angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
 
 
-                        if (!scope.isOpen) {
-                            if (scope.targetMenu.side == "left") {
-                                if (scope.targetMenu.pushContent == "true") {
-                                    angular.element(scope.menuContent).css("transform", "translate3d(" + scope.targetMenu.width + "px, 0px, 0px)");
-                                }
-                                else {
-                                    scope.targetMenu.item.css("transform", "translate3d(" + scope.targetMenu.width + "px, 0px, 0px)");
-                                }
-                            }
-                            else if (scope.targetMenu.side == "right") {
-                                if (scope.targetMenu.pushContent == "true") {
-                                    angular.element(scope.menuContent).css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
+                    // List all menus
+                    angular.forEach(scope.menus, function (item) {
 
-                                }
-                                else {
-                                    scope.targetMenu.item.css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
-                                }
 
-                            }
-                            if (scope.targetMenu.isBackdrop == "true") {
-                                var el = $compile('<div close-aside-menu class="aside-back-drop fade"></div>')(scope);
-                                angular.element(scope.menuContent).append(el);
+                        // Close menu
+                        angular.element(item).css("transform", "translate3d(0px, 0px, 0px)");
 
-                            }
 
-                            scope.isOpen = true;
-
+                        // Get target menu options from attrs
+                        if (angular.element(item).attr("id") == attrs.asideMenuToggle) {
+                            scope.targetMenu.item = angular.element(item);
+                            scope.targetMenu.width = angular.element(item).attr("width");
+                            scope.targetMenu.side = angular.element(item).attr("side");
+                            scope.targetMenu.pushContent = angular.element(item).attr("push-content");
+                            scope.targetMenu.isBackdrop = angular.element(item).attr("is-backdrop");
                         }
-                        else {
+
+
+                    });
+
+
+                    if (!scope.isOpen) {
+
+
+                        if (scope.targetMenu.side == "left") {
+
+
                             if (scope.targetMenu.pushContent == "true") {
-                                angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
+
+
+                                angular.element(scope.menuContent).css("transform", "translate3d(" + scope.targetMenu.width + "px, 0px, 0px)");
+
 
                             }
                             else {
-                                scope.targetMenu.item.css("transform", "translate3d(0px, 0px, 0px)");
+
+
+                                scope.targetMenu.item.css("transform", "translate3d(" + scope.targetMenu.width + "px, 0px, 0px)");
+
+
                             }
-                            scope.isOpen = false;
                         }
-
-                    });
-                }
-            };
-        })
+                        else if (scope.targetMenu.side == "right") {
 
 
-        .directive('asideMenu', function () {
-            'use strict';
-
-            return {
-                restrict: 'EA',
-                link: function (scope, elem, attrs) {
-
-                    angular.element(elem).addClass("aside-menu aside-menu-animate");
+                            if (scope.targetMenu.pushContent == "true") {
 
 
-                    angular.element(elem).css({
-                        "width": attrs.width + "px"
-                    });
+                                angular.element(scope.menuContent).css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
 
 
-                    if (attrs.side == "left") {
+                            }
+                            else {
 
 
-                        angular.element(elem).css("left", 0);
+                                scope.targetMenu.item.css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
 
 
-                        if (attrs.pushContent == "false") {
+                            }
+
+                        }
+                        if (scope.targetMenu.isBackdrop == "true") {
 
 
-                            angular.element(elem).css("z-index", "99");
+                            var el = $compile('<div close-aside-menu class="aside-back-drop fade"></div>')(scope);
 
-                            angular.element(elem).css("left", "-" + attrs.width + "px");
+                            angular.element(scope.menuContent).append(el);
 
 
                         }
 
+                        scope.isOpen = true;
 
                     }
-                    else if (attrs.side == "right") {
+                    else {
 
 
-                        angular.element(elem).css("right", 0);
+                        if (scope.targetMenu.pushContent == "true") {
 
 
-                        if (attrs.pushContent == "false") {
-
-
-                            angular.element(elem).css("z-index", "99");
-
-                            angular.element(elem).css("right", "-" + attrs.width + "px");
+                            angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
 
 
                         }
+                        else {
 
 
-                    }
+                            scope.targetMenu.item.css("transform", "translate3d(0px, 0px, 0px)");
 
 
-                }
-            };
-        })
-
-
-        .directive('asideMenuContent', function () {
-            'use strict';
-
-            return {
-                restrict: 'AE',
-                scope: true,
-                link: function (scope, elem, attrs) {
-
-
-                    angular.element(elem).addClass('aside-menu-content aside-menu-animate close-aside-menu');
-
-
-                }
-            };
-        })
-
-
-        .directive('closeAsideMenu', function () {
-            'use strict';
-
-            return {
-                restrict: 'AC',
-                link: function (scope, elem, attrs) {
-
-
-                    elem.bind('click', function () {
-
-
-                        angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
-
-                        scope.targetMenu.item.css("transform", "translate3d( 0px, 0px, 0px)");
+                        }
 
                         scope.isOpen = false;
+                    }
 
-                        elem.remove();
+                });
+            }
+        };
+    })
 
 
-                    })
+    .directive('asideMenu', function () {
+        'use strict';
+
+        return {
+            restrict: 'EA',
+            link: function (scope, elem, attrs) {
+
+                angular.element(elem).addClass("aside-menu aside-menu-animate");
+
+
+                angular.element(elem).css({"width": attrs.width + "px"});
+
+
+                if (attrs.side == "left") {
+
+
+                    angular.element(elem).css("left", 0);
+
+
+                    if (attrs.pushContent == "false") {
+
+
+                        angular.element(elem).css("z-index", "99");
+
+                        angular.element(elem).css("left", "-" + attrs.width + "px");
+
+
+                    }
 
 
                 }
-            };
-        });
+                else if (attrs.side == "right") {
 
 
-    return module
-}));
+                    angular.element(elem).css("right", 0);
+
+
+                    if (attrs.pushContent == "false") {
+
+
+                        angular.element(elem).css("z-index", "99");
+
+                        angular.element(elem).css("right", "-" + attrs.width + "px");
+
+
+                    }
+
+
+                }
+
+
+            }
+        };
+    })
+
+
+    .directive('asideMenuContent', function () {
+        'use strict';
+
+        return {
+            restrict: 'AE',
+            scope: true,
+            link: function (scope, elem, attrs) {
+
+
+                angular.element(elem).addClass('aside-menu-content aside-menu-animate close-aside-menu');
+
+
+            }
+        };
+    })
+
+
+    .directive('closeAsideMenu', function () {
+        'use strict';
+
+        return {
+            restrict: 'AC',
+            link: function (scope, elem, attrs) {
+
+
+                elem.bind('click', function () {
+
+
+                    angular.element(scope.menuContent).css("transform", "translate3d(0px, 0px, 0px)");
+
+                    scope.targetMenu.item.css("transform", "translate3d( 0px, 0px, 0px)");
+
+                    scope.isOpen = false;
+
+                    elem.remove();
+
+
+                })
+
+
+            }
+        };
+    });
+
+
