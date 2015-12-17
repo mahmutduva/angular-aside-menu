ebf947c093425994e9ca196cd7a92fb485bc18af
İsmail Atkurt <ismail.atkurt@gmail.com>
2015-12-17 15:34:05 +0200

    Revert "list files create for squeeze feature."
    This reverts commit d93c34f6d63fda32a7d13e5e36e124411e554109.

--- dist/aside-menu.js
+++ dist/aside-menu.js
@@ -40,8 +40,7 @@ var module = angular.module('asideModule', [])
                     "width": 275,
                     "side": "left",
                     "pushContentContent": false,
-                    "isBackdrop": false,
-                    "squeeze": false
+                    "isBackdrop": false
                 };
 
 
@@ -67,7 +66,6 @@ var module = angular.module('asideModule', [])
                             scope.targetMenu.side = angular.element(item).attr("side");
                             scope.targetMenu.pushContent = angular.element(item).attr("push-content");
                             scope.targetMenu.isBackdrop = angular.element(item).attr("is-backdrop");
-                            scope.targetMenu.squeeze = angular.element(item).attr("squeeze");
                         }
 
 
@@ -94,15 +92,6 @@ var module = angular.module('asideModule', [])
 
 
                             }
-
-                            if (scope.targetMenu.squeeze == "true") {
-
-
-                                angular.element(scope.menuContent).animate({width: "-=" + scope.targetMenu.width}, 300);
-
-
-                            }
-
                         }
                         else if (scope.targetMenu.side == "right") {
 
@@ -118,14 +107,6 @@ var module = angular.module('asideModule', [])
 
 
                                 scope.targetMenu.item.css("transform", "translate3d(-" + scope.targetMenu.width + "px, 0px, 0px)");
-
-
-                            }
-
-                            if (scope.targetMenu.squeeze == "true") {
-
-
-                                angular.element(scope.menuContent).animate({width: "-=" + scope.targetMenu.width}, 300);
 
 
                             }
@@ -162,14 +143,6 @@ var module = angular.module('asideModule', [])
 
                         }
 
-                        if (scope.targetMenu.squeeze == "true") {
-
-
-                                angular.element(scope.menuContent).animate({width: "+=" + scope.targetMenu.width}, 300);
-
-
-                        }
-                        
                         scope.isOpen = false;
                     }
 
